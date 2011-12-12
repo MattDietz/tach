9564b3b38da1d779cd71042540a837c443e86359
Matt Dietz <matt.dietz@rackspace.com>
2011-12-12 15:56:06 -0600

    Some simple changes to allow for better testing

--- tach/__init__.py
+++ tach/__init__.py
@@ -6,13 +6,20 @@ def _load_config(config_path):
     config = SafeConfigParser()
     config.read(config_path)
     to_decorate = []
+    other_config = None
     for sec in config.sections():
-        module = config.get(sec, 'module')
-        method = config.get(sec, 'method')
-        metric = config.get(sec, 'metric')
-        to_decorate.append((module, method, metric))
-    return to_decorate
+        if sec.startswith('measured.'):
+            module = config.get(sec, 'module')
+            method = config.get(sec, 'method')
+            metric = config.get(sec, 'metric')
+            to_decorate.append((module, method, metric))
+    if config.has_section('graphite.config'):
+        other_config = {'carbon_host': config.get('graphite.config',
+                                                  'carbon_host'),
+                        'carbon_port': config.getint('graphite.config',
+                                               'carbon_port')
+    return to_decorate, other_config
 
 def patch(config_path):
-    for k, m, e in _load_config(config_path):
-        patcher.decorate_method(k, m, e)
+    for k, m, e, o in _load_config(config_path):
+        patcher.decorate_method(k, m, e, o)
--- tach/patcher.py
+++ tach/patcher.py
@@ -1,21 +1,22 @@
 import datetime
+import functools
 
 from tach import decorator
 from tach import utils
 
 
-def _decorate(method, metric):
+def _decorate(method, metric, config):
     metric_method = None
     decorator_name = "_%s" % metric
     metric_method = getattr(decorator, decorator_name)
-
+    metric_method = functools.partial(metric_method, method, config)
     metric_method.__name__ = method.__name__
     metric_method.__doc__ = method.__doc__
     return metric_method
 
-def decorate_method(klass_path, method_name, metric):
+def decorate_method(klass_path, method_name, metric, config):
     kls = utils.import_class(klass_path)
-    method = _decorate(getattr(kls, method_name), metric)
+    method = _decorate(getattr(kls, method_name), metric, config)
     setattr(kls, method_name, method)
 
 
